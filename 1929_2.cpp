862c6a7ebb3f117b0378cb75d0933dcd93b412d7
jumining <76610340+jumining@users.noreply.github.com>
2021-06-29 14:11:14 +0900

    Update 1929_2.cpp

--- 1929_2.cpp
+++ 1929_2.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 using namespace std;
 
-bool isPrime(int n) {
-    if (n < 2) return false;
-    else {
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+void PrimeNum(int arr[],int M, int N) {
+    for(int i=2; i<=N; i++) {
+        arr[i] = i;
+    }
+    arr[0] = arr[1] = 0;
+    for(int i=2; i<=N;i++) {
+        if (arr[i]==0) continue;
+        else {
+            for (int j = i*2; j<=N; j+=i) {
+                arr[j] = 0; //삭제
+            }
         }
-        return true;
+    }
+    for(int i = M; i<=N; i++) {
+        if (arr[i] == 0) continue;
+        else cout << arr[i] << "\n";
     }
 }
 
 int main() {
     int M, N; cin >> M >> N;
-
-    for (int i = M; i <= N; i++) {
-
-        if (isPrime(i)) {
-            cout << i << "\n";
-        }
-
-    }
-
+    int arr[N+1];
+    PrimeNum(arr,M,N);
 }
