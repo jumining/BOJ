3fcd64cc6be3261f7fa937f11d28fa3c28d7a3f1
jumining <76610340+jumining@users.noreply.github.com>
2021-06-30 16:16:40 +0900

    Update DAY3_9020_골드바흐의 추측.cpp

--- DAY3_9020_골드바흐의 추측.cpp
+++ DAY3_9020_골드바흐의 추측.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
 using namespace std;
 
-//골드바흐 파티션 출력 하수
-void Solve(int arr[], int n) {
-    int a = n/2; //a와 b를 더하면 n이 나옴
-    int b = n/2;
-    while(a>0 && b>0) {
-        if (arr[a] == 0 && arr[b] == 0) { //두 수가 소수일 때 까지 반복
-            cout << a << " " << b << "\n";
-            break;
-        }
-        else { a--; b++; }
-    }
-}
 
 int main() {
-    int T; cin >> T; //테스트케이스
+    int T; cin >> T; //테스트 케이스 입력
+    
+    //소수 배열만들기---------
+    int arr[10001] = {0,};
+    for(int i=2; i*i<=10000; i++) {
+        for(int j=i*2; j<=10000; j+=i) {
+            arr[j] = 1;
+        }
+    }
+    //--------------------
+    
     while(T--) {
-        int n; cin >> n; //주어진 수
-        
-        //소수 배열만들기---------
-        int arr[10001] = {0,};
-        arr[0] = arr[1] = 1;
-        for(int i=2; i*i<=10000; i++) {
-            if(arr[i]==1) continue;
-            for(int j=i*2; j<=10000; j+=i) {
-                if(j%i==0) arr[j] = 1;
+        int n; cin >> n; //주어진 수 입력
+        int i,j;
+        for(i=j=n/2;;i++,j--) {
+            if(!arr[i] && !arr[j]) {
+                cout << j << " " << i <<"\n";
+                break;
             }
         }
-        //--------------------
-        
-        Solve(arr, n); //주어진 수 n의 골드바흐 파티션을 출력하는 함수
     }
+    
+    
 }
