bdc0e0a26c9e2e2fde39c13247f0fe0bdb9b012b
jumining <76610340+jumining@users.noreply.github.com>
2021-07-03 18:52:06 +0900

    Create 2312_수 복원하기.cpp

---
+++ DAY6/2312_수 복원하기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        for(int i=2; i<=n; i++) {
+            int cnt{0};
+            while(n%i == 0) {
+                n /= i;
+                cnt++;
+            }
+            if(cnt>0) cout << i << " " << cnt << "\n";
+            
+        }
+    }
+    return 0;
+}
