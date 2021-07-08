f7b169b9ebdf61ba7285c57352a1b6f5b6e3ab2f
jumining <76610340+jumining@users.noreply.github.com>
2021-07-09 01:17:54 +0900

    Create 2003_수들의 합 2.cpp

---
+++ DAY_11/2003_수들의 합 2.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long LL;
+int main(void) {
+    ios::sync_with_stdio(false);
+    LL N, M, answer = 0;
+    cin >> N >> M;
+    LL * A = (LL*)malloc(sizeof(LL)*(N + 1));
+    for (LL n_idx = 1; n_idx <= N; n_idx++)
+        cin >> A[n_idx];
+ 
+    LL left = 1, right = 1, sum = 0;
+    while (left <= right && right <= N + 1) {
+        if (sum >= M) {
+            if (sum == M) answer++;
+            sum -= A[left++];
+        }
+        else {
+            if (right == N + 1) break;
+            sum += A[right++];
+        }
+    }
+    cout << answer;
+    return 0;
+}
